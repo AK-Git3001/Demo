7fb47b0c71e77803f48ea76a0d9c59b69f99b320
kanch <kanch@AMARESHREDDY>
2025-06-27 11:08:26 +0530

    First Push

--- Demo/src/test/java/contactTests/TocreateConTest.java
+++ Demo/src/test/java/contactTests/TocreateConTest.java
@@ -5,6 +5,9 @@ public class TocreateConTest {
 	public static void main(String[] args) {
 	System.out.println("Contact created Successfully");
 
+	System.out.println("Marriage Invitation");
+	System.out.println("Arnold ABEL");
+	System.out.println("Divya");
 	}
 
 }
--- Demo/src/test/java/organizationTests/TocreateOrgTests.java
+++ Demo/src/test/java/organizationTests/TocreateOrgTests.java
@@ -5,6 +5,9 @@ public class TocreateOrgTests {
 	public static void main(String[] args) {
 		System.out.println("Organisation created successfully");
 
+		System.out.println("Marriage Invitation");
+		System.out.println("Arnold ABEL");
+		System.out.println("Divya");
 	}
 
 }
